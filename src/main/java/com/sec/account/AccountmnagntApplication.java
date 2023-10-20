2d7b35d2953edccc76d442e7feed9e2a8bcb1a66
TyagiAnkit1697 <ankittyagi1697@gmail.com>
2023-10-20 09:53:27 +0530

    initial push

---
+++ src/main/java/com/sec/account/AccountmnagntApplication.java
@@ -0,0 +1,13 @@
+package com.sec.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmnagntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmnagntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sec/account/AccountmnagntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmnagntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
